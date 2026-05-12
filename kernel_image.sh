a64b5a1b700ffa11e3c17f0c13db11ce9b24dd9d
taniguchi <assnsmile2005@gmail.com>
2026-05-12 15:28:17 +0900

    changed kernel version to ubuntu24.04.4

--- kernel_image.sh
+++ kernel_image.sh
@@ -1,9 +1,9 @@
 sudo apt update;
 sudo apt install qemu-system-misc;
 sudo apt install u-boot-qemu;
-wget https://cdimage.ubuntu.com/releases/24.04/release/ubuntu-24.04.3-preinstalled-server-riscv64.img.xz;
-xz -dk ubuntu-24.04.3-preinstalled-server-riscv64.img.xz;
-qemu-img info ubuntu-24.04.3-preinstalled-server-riscv64.img;
-qemu-img resize ubuntu-24.04.3-preinstalled-server-riscv64.img +10G;
+wget https://cdimage.ubuntu.com/releases/24.04/release/ubuntu-24.04.4-preinstalled-server-riscv64.img.xz;
+xz -dk ubuntu-24.04.4-preinstalled-server-riscv64.img.xz;
+qemu-img info ubuntu-24.04.4-preinstalled-server-riscv64.img;
+qemu-img resize ubuntu-24.04.4-preinstalled-server-riscv64.img +10G;
 sudo apt install qemu-user-static binfmt-support
 
