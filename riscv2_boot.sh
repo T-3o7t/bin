423039e32fc7812a5fb243d92cbbd014543709d6
Ta <assnsmile2005@gmail.com>
2026-09-10 09:17:20 +0900

    boot flow command for riscv2

---
+++ riscv2_boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 1. USBデバイスをWSLにアタッチ（管理者権限が必要なため昇格して実行）
+powershell.exe -Command "Start-Process powershell -Verb RunAs -ArgumentList '-Command usbipd attach --wsl --busid 3-1; usbipd list'"
+
+# 2. シリアル通信
+sudo minicom
+
+# 3. ファイルコピー
+powershell.exe -Command "Copy-Item 'C:/Users/Lab_student/Desktop/bootloader_addr5_secboot.bin' -Destination 'D:/'"
